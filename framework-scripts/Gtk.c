35009c6a4ac129b8527d03fb6bdeca29bdf0ffc1
Kristian Rietveld <kris@koe-lo-yuk.local>
2008-01-17 13:24:26 +0100

    Update framework initializer to set more environment variables; update TODO.

--- framework-scripts/Gtk.c
+++ framework-scripts/Gtk.c
@@ -9,14 +9,33 @@
 #include <string.h>
 #include <unistd.h>
 
-#define RELATIVE_RC_FILE "../Frameworks/Gtk.framework/Resources/etc/pango/pangorc"
+#define BUNDLE_RESOURCES "../Frameworks/Gtk.framework/Resources"
+
+#define RELATIVE_PANGO_RC_FILE		BUNDLE_RESOURCES"/etc/pango/pangorc"
+#define RELATIVE_GTK_RC_FILE		BUNDLE_RESOURCES"/etc/gtk-2.0/gtkrc"
+#define RELATIVE_GTK_IMMODULE_FILE	BUNDLE_RESOURCES"/etc/gtk-2.0/gtk.immodules"
+#define RELATIVE_GDK_PIXBUF_MODULE_FILE	BUNDLE_RESOURCES"/etc/gtk-2.0/gdk-pixbuf.loaders"
+
+
+static void set_rc_environment (const char *variable,
+                                const char *executable_path,
+                                const char *relative_file)
+{
+  char *rc_file;
+
+  rc_file = malloc (strlen (executable_path) + strlen (relative_file));
+  sprintf (rc_file, "%s%s", executable_path, relative_file);
+
+  setenv (variable, rc_file, 1);
+
+  free (rc_file);
+}
 
 __attribute__((constructor))
 static void initializer (int argc, char **argv, char **envp)
 {
   int i;
   char *executable_path;
-  char *pango_rc;
 
   /* We figure out the executable path and then set up the PANGO_RC_FILE
    * environment variable.
@@ -30,11 +49,16 @@ static void initializer (int argc, char **argv, char **envp)
       executable_path[i] = 0;
     }
 
-  pango_rc = malloc (strlen (executable_path) + strlen (RELATIVE_RC_FILE));
-  sprintf (pango_rc, "%s%s", executable_path, RELATIVE_RC_FILE);
+  setenv ("XDG_CONFIG_DIRS", BUNDLE_RESOURCES"/etc/xdg", 1);
+  setenv ("XDG_DATA_DIRS", BUNDLE_RESOURCES"/share", 1);
+  setenv ("GTK_DATA_PREFIX", BUNDLE_RESOURCES"/share", 1);
+  setenv ("GTK_EXE_PREFIX", BUNDLE_RESOURCES, 1);
+  setenv ("GTK_PATH", BUNDLE_RESOURCES, 1);
 
-  setenv ("PANGO_RC_FILE", pango_rc, 1);
+  set_rc_environment ("PANGO_RC_FILE", executable_path, RELATIVE_PANGO_RC_FILE);
+  set_rc_environment ("GTK2_RC_FILES", executable_path, RELATIVE_GTK_RC_FILE);
+  set_rc_environment ("GTK_IM_MODULE_FILE", executable_path, RELATIVE_GTK_IMMODULE_FILE);
+  set_rc_environment ("GDK_PIXBUF_MODULE_FILE", executable_path, RELATIVE_GDK_PIXBUF_MODULE_FILE);
 
-  free (pango_rc);
   free (executable_path);
 }
