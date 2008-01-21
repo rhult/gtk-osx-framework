270b080b36cccd4dce24bca67ea72cf9804bca21
Kristian Rietveld <kris@koe-lo-yuk.local>
2008-01-21 10:17:55 +0100

    Modify initializer to detect whether we are running against a bundled or the system framework and set the environment variables according.

--- framework-scripts/Gtk.c
+++ framework-scripts/Gtk.c
@@ -8,61 +8,99 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <mach-o/dyld.h>
 
-#define BUNDLE_RESOURCES "../Frameworks/Gtk.framework/Resources"
+#include <glib.h>
 
-#define RELATIVE_PANGO_RC_FILE		BUNDLE_RESOURCES"/etc/pango/pangorc"
-#define RELATIVE_GTK_RC_FILE		BUNDLE_RESOURCES"/etc/gtk-2.0/gtkrc"
-#define RELATIVE_GTK_IMMODULE_FILE	BUNDLE_RESOURCES"/etc/gtk-2.0/gtk.immodules"
-#define RELATIVE_GDK_PIXBUF_MODULE_FILE	BUNDLE_RESOURCES"/etc/gtk-2.0/gdk-pixbuf.loaders"
+/* Relative location of the framework in an application bundle */
+#define FRAMEWORK_OFFSET "../Frameworks/Gtk.framework"
+
+#define RELATIVE_PANGO_RC_FILE			"/Resources/etc/pango/pangorc"
+#define RELATIVE_GTK_RC_FILE			"/Resources/etc/gtk-2.0/gtkrc"
+#define RELATIVE_GTK_IMMODULE_FILE		"/Resources/etc/gtk-2.0/gtk.immodules"
+#define RELATIVE_GDK_PIXBUF_MODULE_FILE		"/Resources/etc/gtk-2.0/gdk-pixbuf.loaders"
 
 
 static void set_rc_environment (const char *variable,
-                                const char *executable_path,
+                                const char *bundle_prefix,
                                 const char *relative_file)
 {
   char *rc_file;
 
-  rc_file = malloc (strlen (executable_path) + strlen (relative_file));
-  sprintf (rc_file, "%s%s", executable_path, relative_file);
-
+  rc_file = g_strdup_printf ("%s%s", bundle_prefix, relative_file);
   setenv (variable, rc_file, 1);
+  g_free (rc_file);
+}
 
-  free (rc_file);
+static int is_running_from_app_bundle (void)
+{
+  int i;
+
+  /* Here we use some dyld magic to figure out whether we are running
+   * against the system's Gtk.framework or a Gtk.framework found bundled
+   * in the application bundle.
+   */
+
+  for (i = 0; i < _dyld_image_count (); i++)
+    {
+      const char *name = _dyld_get_image_name (i);
+
+      if (strstr (name, "Gtk.framework/Gtk"))
+        {
+          if (!strncmp (name, "/Library/", 9))
+            return 0;
+          else
+            return 1;
+        }
+    }
+
+  /* shouldn't be reached ... */
+  return 0;
 }
 
 __attribute__((constructor))
 static void initializer (int argc, char **argv, char **envp)
 {
   int i;
-  char *executable_path;
+  char *bundle_prefix;
 
-  /* We figure out the executable path and then set up the PANGO_RC_FILE
-   * environment variable.
-   */
-  executable_path = strdup (argv[0]);
-  for (i = strlen (executable_path); i >= 0; i--)
+  /* Figure out correct bundle prefix */
+  if (!is_running_from_app_bundle ())
     {
-      if (executable_path[i] == '/')
-	break;
+      bundle_prefix = g_strdup ("/Library/Frameworks/Gtk.framework");
+    }
+  else
+    {
+      char *executable_path;
 
-      executable_path[i] = 0;
+      executable_path = g_strdup (argv[0]);
+      for (i = strlen (executable_path); i >= 0; i--)
+        {
+          if (executable_path[i] == '/')
+            break;
+
+          executable_path[i] = 0;
+        }
+
+      bundle_prefix = g_strdup_printf ("%s/%s", executable_path, FRAMEWORK_OFFSET);
+
+      g_free (executable_path);
     }
 
   /* NOTE: leave the setting of the environment variables in this order,
    * otherwise things will fail in the Release builds for no obvious
    * reason.
    */
-  set_rc_environment ("PANGO_RC_FILE", executable_path, RELATIVE_PANGO_RC_FILE);
-  set_rc_environment ("GTK2_RC_FILES", executable_path, RELATIVE_GTK_RC_FILE);
-  set_rc_environment ("GTK_IM_MODULE_FILE", executable_path, RELATIVE_GTK_IMMODULE_FILE);
-  set_rc_environment ("GDK_PIXBUF_MODULE_FILE", executable_path, RELATIVE_GDK_PIXBUF_MODULE_FILE);
+  set_rc_environment ("PANGO_RC_FILE", bundle_prefix, RELATIVE_PANGO_RC_FILE);
+  set_rc_environment ("GTK2_RC_FILES", bundle_prefix, RELATIVE_GTK_RC_FILE);
+  set_rc_environment ("GTK_IM_MODULE_FILE", bundle_prefix, RELATIVE_GTK_IMMODULE_FILE);
+  set_rc_environment ("GDK_PIXBUF_MODULE_FILE", bundle_prefix, RELATIVE_GDK_PIXBUF_MODULE_FILE);
 
-  setenv ("XDG_CONFIG_DIRS", BUNDLE_RESOURCES"/etc/xdg", 1);
-  setenv ("XDG_DATA_DIRS", BUNDLE_RESOURCES"/share", 1);
-  setenv ("GTK_DATA_PREFIX", BUNDLE_RESOURCES"/share", 1);
-  setenv ("GTK_EXE_PREFIX", BUNDLE_RESOURCES, 1);
-  setenv ("GTK_PATH", BUNDLE_RESOURCES, 1);
+  set_rc_environment ("XDG_CONFIG_DIRS", bundle_prefix, "/Resources/etc/xdg");
+  set_rc_environment ("XDG_DATA_DIRS", bundle_prefix, "/Resources/share");
+  set_rc_environment ("GTK_DATA_PREFIX", bundle_prefix, "/Resources/share");
+  set_rc_environment ("GTK_EXE_PREFIX", bundle_prefix, "/Resources");
+  set_rc_environment ("GTK_PATH", bundle_prefix, "/Resources");
 
-  free (executable_path);
+  g_free (bundle_prefix);
 }
