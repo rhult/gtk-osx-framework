33b955f56d36bca731da8e252207e1034e61c992
Richard Hult <richard@imendio.com>
2008-04-13 14:46:46 +0200

    Make Gtk.c build outside jhbuild, and don't use glib before we are initialized
    Use libc string functions instead of glib, get rid of glib/gtk header
    includes. Also follow Apple docs and guard against multiple
    initialization.

--- framework-scripts/Gtk.c
+++ framework-scripts/Gtk.c
@@ -11,44 +11,48 @@
 #include <mach-o/dyld.h>
 #include <Carbon/Carbon.h>
 
-#include <glib.h>
-#include <gtk/gtk.h>
-
 /* Relative location of the framework in an application bundle */
-#define FRAMEWORK_OFFSET "../Frameworks/Gtk.framework"
+#define FRAMEWORK_OFFSET                        "../Frameworks/Gtk.framework"
 
 #define RELATIVE_PANGO_RC_FILE			"/Resources/etc/pango/pangorc"
 #define RELATIVE_GTK_RC_FILE			"/Resources/etc/gtk-2.0/gtkrc"
 #define RELATIVE_GTK_IMMODULE_FILE		"/Resources/etc/gtk-2.0/gtk.immodules"
 #define RELATIVE_GDK_PIXBUF_MODULE_FILE		"/Resources/etc/gtk-2.0/gdk-pixbuf.loaders"
 
-/* Define these private functions from ige-mac-integration here, so we don't
- * have to install a private header.
- */
-gboolean _ige_mac_menu_is_quit_menu_item_handled (void);
-gboolean _ige_mac_dock_is_quit_menu_item_handled (void);
+/* Define those so we can build this outside of jhbuild. */
+int  _ige_mac_menu_is_quit_menu_item_handled (void);
+int  _ige_mac_dock_is_quit_menu_item_handled (void);
+void gtk_main_quit                           (void);
 
 /* Make the bundle prefix available to the outside */
 static char *bundle_prefix = NULL;
 
-const char *_gtk_framework_get_bundle_prefix (void)
+const char *
+_gtk_framework_get_bundle_prefix (void)
 {
   return bundle_prefix;
 }
 
 
-static void set_rc_environment (const char *variable,
-                                const char *bundle_prefix,
-                                const char *relative_file)
+static void
+set_rc_environment (const char *variable,
+                    const char *bundle_prefix,
+                    const char *relative_file)
 {
   char *rc_file;
 
-  rc_file = g_strdup_printf ("%s%s", bundle_prefix, relative_file);
-  setenv (variable, rc_file, 1);
-  g_free (rc_file);
+  rc_file = malloc (strlen (bundle_prefix) + strlen (relative_file) + 1);
+  strcpy (rc_file, bundle_prefix);
+  strcpy (rc_file + strlen (bundle_prefix), relative_file);
+
+  if (setenv (variable, rc_file, 1) < 0)
+    perror ("Couldn't set environment variable");
+
+  free (rc_file);
 }
 
-static int is_running_from_app_bundle (void)
+static int
+is_running_from_app_bundle (void)
 {
   int i;
 
@@ -70,7 +74,7 @@ static int is_running_from_app_bundle (void)
         }
     }
 
-  /* shouldn't be reached ... */
+  /* Shouldn't be reached ... */
   return 0;
 }
 
@@ -87,21 +91,26 @@ handle_quit_cb (const AppleEvent *inAppleEvent,
   return noErr;
 }
 
-__attribute__((constructor))
-static void initializer (int argc, char **argv, char **envp)
+__attribute__((constructor)) static void
+initializer (int argc, char **argv, char **envp)
 {
+  static initialized = 0;
   int i;
+
+  if (initialized)
+    return;
+  initialized = 1;
 
   /* Figure out correct bundle prefix */
   if (!is_running_from_app_bundle ())
     {
-      bundle_prefix = g_strdup ("/Library/Frameworks/Gtk.framework");
+      bundle_prefix = strdup ("/Library/Frameworks/Gtk.framework");
     }
   else
     {
       char *executable_path;
 
-      executable_path = g_strdup (argv[0]);
+      executable_path = strdup (argv[0]);
       for (i = strlen (executable_path); i >= 0; i--)
         {
           if (executable_path[i] == '/')
@@ -110,9 +119,12 @@ static void initializer (int argc, char **argv, char **envp)
           executable_path[i] = 0;
         }
 
-      bundle_prefix = g_strdup_printf ("%s/%s", executable_path, FRAMEWORK_OFFSET);
+      bundle_prefix = malloc (strlen (executable_path) + 1 + strlen (FRAMEWORK_OFFSET) + 1);
+      strcpy (bundle_prefix, executable_path);
+      strcpy (bundle_prefix + strlen (executable_path), "/");
+      strcpy (bundle_prefix + strlen (executable_path) + 1, FRAMEWORK_OFFSET);
 
-      g_free (executable_path);
+      free (executable_path);
     }
 
   /* NOTE: leave the setting of the environment variables in this order,
@@ -130,8 +142,8 @@ static void initializer (int argc, char **argv, char **envp)
   set_rc_environment ("GTK_EXE_PREFIX", bundle_prefix, "/Resources");
   set_rc_environment ("GTK_PATH", bundle_prefix, "/Resources");
 
-  /* We don't free bundle_prefix; it needs to be available until
-   * program termination.
+  /* We don't free bundle_prefix; it needs to be available until program
+   * termination.
    */
 
   /* Handle quit menu item so that apps can be quit of of the box. */
