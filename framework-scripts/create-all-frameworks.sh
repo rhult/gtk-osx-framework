e39d88ece2f5ceb44bc2acabad01ce85de55ea7e
Richard Hult <richard@imendio.com>
2008-05-01 23:15:45 +0200

    Add command line options to script

--- framework-scripts/create-all-frameworks.sh
+++ framework-scripts/create-all-frameworks.sh
@@ -28,48 +28,72 @@ case "$PREFIX" in
         ;;
 esac
 
-# Make it possble to not update modules, makes for quicker rebuilding.
-if [ x$SKIP_UPDATE != x -o x$SKIP_BUILD != x ]; then
-    SKIP_UPDATE="-n"
-fi
-
-build()
+print_usage()
 {
-    if [ x$SKIP_BUILD == x ]; then
-        rm "$PREFIX"/lib/*.la 2>/dev/null
-        jhbuild buildone $SKIP_UPDATE -c $* || exit 1
-    fi
+    echo "Usage: `basename $0` [-fnlh] [FRAMEWORK...]"
+    echo "Options:"
+    echo "  -f        - Do not rebuild, just recreate the frameworks"
+    echo "  -n        - Do not update modules (no network mode)"
+    echo "  -l        - Do not run 'make clean' before building"
+    echo "  -h        - Display this help text"
+    echo "            - FRAMEWORK... is an optional list of frameworks to create"
 }
 
 create_framework()
 {
-    rm -rf $1.framework
-    ./create-$1-framework.sh $PREFIX || exit 1
+    framework=$1
+    shift 1
+
+    if (echo "$modules" | grep -w $framework) >/dev/null; then
+        if [ $rebuild == yes ]; then
+            rm "$PREFIX"/lib/*.la 2>/dev/null
+            jhbuild buildone $update $clean $* || exit 1
+        fi
+
+        rm -rf $framework.framework
+        ./create-$framework-framework.sh $PREFIX || exit 1
+    fi
 
     if [ "x$JHB_PREPEND_FRAMEWORKS" == x ]; then
-        export JHB_PREPEND_FRAMEWORKS=`pwd`/$1.framework
+        export JHB_PREPEND_FRAMEWORKS=`pwd`/$framework.framework
     else
-        export JHB_PREPEND_FRAMEWORKS="$JHB_PREPEND_FRAMEWORKS:`pwd`/$1.framework"
+        export JHB_PREPEND_FRAMEWORKS="$JHB_PREPEND_FRAMEWORKS:`pwd`/$framework.framework"
     fi
 }
 
-build intltool
-build glib
-create_framework GLib
+rebuild=yes
+update=
+clean=-c
+while getopts "fnh" o; do
+    case "$o" in
+        f)
+            rebuild=no
+            ;;
+        n)
+            update=-n
+            ;;
+        l)
+            clean=
+            ;;
+        h)
+            print_usage
+            exit 0
+            ;;
+        ?)
+            print_usage
+            exit 1
+            ;;
+    esac
+done
+shift $(($OPTIND - 1))
+modules=$*
 
-build pixman
-build cairo
-create_framework Cairo
+create_framework GLib intltool glib
 
-build atk
-build pango
-build gtk+
-build gtk-engines
-build ige-mac-integration
-create_framework Gtk
+create_framework Cairo pixman cairo
 
-build libglade
-create_framework Libglade
+create_framework Gtk atk pango gtk+ gtk-engines ige-mac-integration
 
-build loudmouth
-create_framework Loudmouth
+create_framework Libglade libglade
+
+create_framework Loudmouth loudmouth
